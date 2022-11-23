63c726b53920a095667e6f2aac4250269b674655
RHsanto <rakibulsanto789@gmail.com>
2022-11-23 23:43:43 +0600

    Add

--- index.js
+++ index.js
@@ -123,6 +123,25 @@ async function run() {
       const bus = await CollectionBus.findOne(query);
       res.json(bus);
     });
+
+
+    app.use((req, res, next) => {
+      throw Error("Requested content wasn't found");
+    });
+    
+    app.use((err, req, res, next) => {
+      if(req.headerSent){
+          next('Something went wrong!');
+      }
+      else{
+          if(err.message){
+              res.status(404).json({success: false, message: err.message})
+          }  else{
+              res.status(500).json({success: false, message: "There was an internal problem"})
+          }
+      }
+    })
+    
   } finally {
     // await client.close();
   }
@@ -130,27 +149,11 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Running the server on Mr. Trave Travel 23");
-});
+// app.get("/", (req, res) => {
+//   res.send("Running the server on Mr. Trave Travel 23");
+// });
 
 // 
-app.use((req, res, next) => {
-  throw Error("Requested content wasn't found");
-});
-
-app.use((err, req, res, next) => {
-  if(req.headerSent){
-      next('Something went wrong!');
-  }
-  else{
-      if(err.message){
-          res.status(404).json({success: false, message: err.message})
-      }  else{
-          res.status(500).json({success: false, message: "There was an internal problem"})
-      }
-  }
-})
 
 
 app.listen(port, () => {
